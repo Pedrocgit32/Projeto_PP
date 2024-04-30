473a51992fc226ab8574cbc58dd9f1b520087be4
Pedro <arquivos.pedro1948@gmail.com>
2024-04-30 11:52:47 -0300

    Api pronta e fdp funcionando

--- front/script.js
+++ front/script.js
@@ -8,7 +8,7 @@ button.onclick = async function() {
     let cep = document.getElementById("cep").value;
     let data = {nome,email,senha,cpf,cep}
 
-    const reseponse = await fetch('http://localhost:3005/api/store/task', {
+    const response = await fetch('http://localhost:3005/api/store/task', {
         method: "POST",
         headers: {"Content-type": "application/json;charset=UTF-8"},
         body: JSON.stringify(data)
@@ -17,32 +17,8 @@ button.onclick = async function() {
     let content = await response.json();
 
     if(content.success) {
-        alert("Sucesso")
-    } else {
-        alert('Não');
-    }
-}
-
-let button = document.getElementById("entrar");
-
-button.onclick = async function() {
-    let nome = document.getElementById("nome").value;
-    let email = document.getElementById("email").value;
-    let senha = document.getElementById("senha").value;
-    let cpf = document.getElementById("cpf").value;
-    let cep = document.getElementById("cep").value;
-    let data = {nome,email,senha,cpf,cep}
-
-    const reseponse = await fetch('http://localhost:3005/api/store/task', {
-        method: "GET",
-        headers: {"Content-type": "application/json;charset=UTF-8"},
-        body: JSON.stringify(data)
-    });
-
-    let content = await response.json();
-
-    if(content.success) {
-        alert("Sucesso")
+        // alert("Sucesso")
+        window.location.pathname = "/front/foto_perfil.html"
     } else {
         alert('Não');
     }
