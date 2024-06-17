3cee45c9dfac9e16734d3e51ad5935b02877555e
Pedro <arquivos.pedro1948@gmail.com>
2024-06-17 12:16:56 -0300

    tentando arrumar o mapa

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
-const taskRouter = require('./routes/taskRouter');
 const cors = require('cors');
 const app = express();
+
+const taskRouter = require('./routes/taskRouter');
+const loginRouter = require('./routes/loginRouter');
 
 app.set('port', process.env.PORT || 3005);
 app.use(express.json());
 app.use(cors());
-app.use('/api', taskRouter);
+app.use('/api', [
+
+    taskRouter, 
+    loginRouter]);
 
 module.exports = app;
--- front/script_login.js
+++ front/script_login.js
@@ -1,5 +1,23 @@
-let button = document.getElementById("fsc")
-
-button.onclick = async function() {
-    window.location.pathname = "/front/Cadastro.html"
-};
+async function handleSubmit(event) {
+    event.preventDefault();
+  
+    const email    = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
+  
+    const data = {email, senha};
+  
+    const response = await fetch("http://localhost:3005/api/login", {
+      method: "POST",
+      headers: {"Content-Type":"application/json;charset=UTF-8"},
+      body: JSON.stringify(data)
+    });
+  
+    const result = await response.json();
+  
+    if (result.success) {
+      console.log(result.data)
+      window.location.href = "/front/Cadastro.html";
+    } else {
+      alert(result.message);
+    }
+  };
