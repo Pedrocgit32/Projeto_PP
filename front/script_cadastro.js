795313f5e324b0455fdfb3fc30893702adf9a578
Pedro <arquivos.pedro1948@gmail.com>
2024-10-02 12:21:00 -0300

    em andamento

--- front/script_cadastro.js
+++ front/script_cadastro.js
@@ -8,19 +8,29 @@ button.onclick = async function() {
     let cep = document.getElementById("cep").value;
     let data = {nome,email,senha,cpf,cep}
 
-    const response = await fetch('http://localhost:3005/api/store/cadastro', {
-        method: "POST",
-        headers: {"Content-type": "application/json;charset=UTF-8"},
-        body: JSON.stringify(data)
-    });
+    
 
-    let content = await response.json();
+    if(cep[0] == 9 && cep[1] == 3 && cep[2] == 0 ){
+  
+        const response = await fetch('http://localhost:3005/api/store/cadastro', {
+            method: "POST",
+            headers: {"Content-type": "application/json;charset=UTF-8"},
+            body: JSON.stringify(data)
+        });
 
-    if(content.success) {
+        let content = await response.json();
+
+        if(content.success) {
         // alert("Sucesso")
-        window.location.pathname = "/front/index_login.html"
-    } else {
-        alert('Não deu');
+            window.location.pathname = "/front/index_login.html"
+        } else {
+            alert('Não deu');
+        }
+
+    }else{
+        alert("CEP inválido.")
+        window.location.pathname = "/front/Cadastro.html"
     }
+    
 }
 
