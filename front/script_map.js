ac7ad1af23f773a8a66430c31f4a1293d8d72325
PEDRO CARDOSO <04657220080@senacrs.edu.br>
2024-10-04 11:48:31 -0300

    back-end arrumado

--- front/script_map.js
+++ front/script_map.js
@@ -6,62 +6,112 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-const marker = L.marker([-29.757926, -51.149658]).addTo(map);
-const marker2 = L.marker([-29.757164, -51.155728]).addTo(map);
-const marker3 = L.marker([-29.756281, -51.144337]).addTo(map);
-const marker4 = L.marker([-29.758440, -51.153014]).addTo(map);
+// Array com as coordenadas dos marcadores
+const markersData = [
+    { coords: [-29.757926, -51.149658], popupIndex: 0 },
+    { coords: [-29.757164, -51.155728], popupIndex: 1 },
+    { coords: [-29.756281, -51.144337], popupIndex: 2 },
+    { coords: [-29.758440, -51.153014], popupIndex: 3 }
+];
 
-const defaultPopupContent = `
-    <b>Carregando informações...</b><br>
-    <p>Aguarde até que os dados sejam recuperados.</p>
-`;
+// Array para armazenar os marcadores
+const markers = markersData.map(markerData => L.marker(markerData.coords).addTo(map));
 
-// Faz a requisição para a API para recuperar a imagem e o comentário
 async function getFeed() {
     const images = 'http://localhost:3005/uploads/';
     const response = await fetch('http://localhost:3005/api/posts', {
         method: 'GET',
         headers: {
-            "Content-Type":"application/json"
+            "Content-Type": "application/json"
         }
-    })
+    });
 
     const results = await response.json();
-  
-    if(results.success) {
+
+    if (results.success) {
         let feeds = await results.data;
-        // console.log(feeds)
 
-        const popupContent = `
-            <b>${feeds[0].comment}</b><br>
-            <img src="${images + feeds[0].file}" alt="Imagem do local" style="width:100px;height:auto;">
-            
-        `;
-        
-        const popupContent2 = `
-            <b>${feeds[1].comment}</b><br>
-            <img src="${images + feeds[1].file}" alt="Imagem do local" style="width:100px;height:auto;">
-        `;
-        
-        const popupContent3 = `
-            <b>${feeds[2].comment}</b><br>
-            <img src="${images + feeds[2].file}" alt="Imagem do local" style="width:100px;height:auto;">
-        `;
-        const popupContent4 = `
-            <b>${feeds[3].comment}</b><br>
-            <img src="${images + feeds[3].file}" alt="Imagem do local" style="width:100px;height:auto;">
-        `;
-        
-        
-
-
-        marker.bindPopup(popupContent).openPopup();
-        marker2.bindPopup(popupContent2).openPopup();
-        marker3.bindPopup(popupContent3).openPopup();
-        marker4.bindPopup(popupContent4).openPopup();
+        // Itera sobre os marcadores e associa os popups
+        markers.forEach((marker, index) => {
+            const popupContent = `
+                <b>${feeds[index].comment}</b><br>
+                <img src="${images + feeds[index].file}" alt="Imagem do local" style="width:100px;height:auto;">
+            `;
+            marker.bindPopup(popupContent).openPopup();
+        });
     } else {
         alert('Ops');
-    
-}}
+    }
+}
 
 getFeed();
+
+
+
+
+
+
+// // Cria o mapa e define a visualização inicial
+// var map = L.map('map').setView([-29.75831429389668, -51.15101134694835], 13);
+
+// // Adiciona o tile layer do OpenStreetMap
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(map);
+
+// const marker = L.marker([-29.757926, -51.149658]).addTo(map);
+// const marker2 = L.marker([-29.757164, -51.155728]).addTo(map);
+// const marker3 = L.marker([-29.756281, -51.144337]).addTo(map);
+// const marker4 = L.marker([-29.758440, -51.153014]).addTo(map);
+
+
+
+// // Faz a requisição para a API para recuperar a imagem e o comentário
+// async function getFeed() {
+//     const images = 'http://localhost:3005/uploads/';
+//     const response = await fetch('http://localhost:3005/api/posts', {
+//         method: 'GET',
+//         headers: {
+//             "Content-Type":"application/json"
+//         }
+//     })
+
+//     const results = await response.json();
+  
+//     if(results.success) {
+//         let feeds = await results.data;
+//         // console.log(feeds)
+
+//         const popupContent = `
+//             <b>${feeds[0].comment}</b><br>
+//             <img src="${images + feeds[0].file}" alt="Imagem do local" style="width:100px;height:auto;">
+            
+//         `;
+        
+//         const popupContent2 = `
+//             <b>${feeds[1].comment}</b><br>
+//             <img src="${images + feeds[1].file}" alt="Imagem do local" style="width:100px;height:auto;">
+//         `;
+        
+//         const popupContent3 = `
+//             <b>${feeds[2].comment}</b><br>
+//             <img src="${images + feeds[2].file}" alt="Imagem do local" style="width:100px;height:auto;">
+//         `;
+//         const popupContent4 = `
+//             <b>${feeds[3].comment}</b><br>
+//             <img src="${images + feeds[3].file}" alt="Imagem do local" style="width:100px;height:auto;">
+//         `;
+        
+        
+
+
+//         marker.bindPopup(popupContent).openPopup();
+//         marker2.bindPopup(popupContent2).openPopup();
+//         marker3.bindPopup(popupContent3).openPopup();
+//         marker4.bindPopup(popupContent4).openPopup();
+//     } else {
+//         alert('Ops');
+    
+// }}
+
+// getFeed();
